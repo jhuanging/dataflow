cf2590576778c435c7d2601fd5c498c51fc929ef
qinyuguang <qinyuguang@gmail.com>
2018-04-08 00:57:45 +0800

    fix start shell

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 chown www-data:www-data -R /home/logs/project /home/logs/php
 
-/usr/local/bin/php /www/dataflow/src/public/console/index.php request_uri=/transfer start -d
+/usr/local/bin/php /www/dataflow/src/public/console/index.php request_uri=/transfer start
 
